fac6b2d965aee221efe4874c42caab8a5512662d
Serhan Sarac <serhansarac1980@gmail.com>
2025-02-06 15:05:37 -0500

    Fixed static files line endings

--- ShipRepairPlatform60/settings.py
+++ ShipRepairPlatform60/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,5 +130,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Production için tüm statik dosyalar buraya toplanır
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Development için ek statik dosya dizinleri
+]
