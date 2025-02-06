bdb1d0ad928610bbdb085093f8f94811abdc5d3e
Serhan Sarac <serhansarac1980@gmail.com>
2025-02-06 07:11:29 -0500

    Updated start script

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt update && apt install -y libgobject-2.0-0 libcairo2 libffi-dev libpango1.0-dev
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn ShipRepairPlatform60.wsgi:application --bind 0.0.0.0:8000
